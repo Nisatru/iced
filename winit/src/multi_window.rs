ea695cf48e9bf01ebfde662bd1de655223c6a659
Nisatru <thomasbirke@gmx.de>
2024-02-10 20:01:24 +0000

    Comment out more macos specific events

--- winit/src/multi_window.rs
+++ winit/src/multi_window.rs
@@ -430,22 +430,22 @@ async fn run_instance<A, E, C>(
                             window.raw.request_redraw();
                         }
                     }
-                    event::Event::PlatformSpecific(
-                        event::PlatformSpecific::MacOS(
-                            event::MacOS::ReceivedUrl(url),
-                        ),
-                    ) => {
-                        use crate::core::event;
+                    // event::Event::PlatformSpecific(
+                    //     event::PlatformSpecific::MacOS(
+                    //         event::MacOS::ReceivedUrl(url),
+                    //     ),
+                    // ) => {
+                    //     use crate::core::event;
 
-                        events.push((
-                            None,
-                            event::Event::PlatformSpecific(
-                                event::PlatformSpecific::MacOS(
-                                    event::MacOS::ReceivedUrl(url),
-                                ),
-                            ),
-                        ));
-                    }
+                    //     events.push((
+                    //         None,
+                    //         event::Event::PlatformSpecific(
+                    //             event::PlatformSpecific::MacOS(
+                    //                 event::MacOS::ReceivedUrl(url),
+                    //             ),
+                    //         ),
+                    //     ));
+                    // }
                     event::Event::UserEvent(message) => {
                         messages.push(message);
                     }
